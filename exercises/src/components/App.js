efefb666b0ce6e9c94180e69117854dc244d6893
slawomir.wojciga@ge.com <503271002@ge.com>
2022-08-23 15:21:37 +0200

    reorganize app structure

--- exercises/src/components/App.js
+++ exercises/src/components/App.js
@@ -2,42 +2,14 @@ import './App.css';
 
 import React, { Component } from 'react';
 
-import SwitchButton from './SwitchButton';
-
+import Counter from './Counter/Counter';
 
 class App extends Component {
-
-  idInterval = '';
-
-  state = {
-    time: 0,
-    active: false
-  }
-
-  addSecond = () => {
-    this.setState((prevState) => {
-      return { time: prevState.time + 1 }
-    })
-  }
-
-  handleClick = () => {
-    if (this.state.active) {
-      clearInterval(this.idInterval)
-    } else {
-      this.idInterval = setInterval(() => this.addSecond(), 1000);
-    }
-
-    this.setState({
-      active: !this.state.active
-    });
-
-  }
 
   render() {
     return (
       <>
-        <p>{this.state.time}</p>
-        <SwitchButton click={this.handleClick} active={this.state.active}/>
+        <Counter />
       </>
     );
   }
---
+++ exercises/src/components/Counter/Counter.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+import SwitchButton from './SwitchButton';
+
+class Counter extends Component {
+
+    idInterval = '';
+
+    state = {
+      time: 0,
+      active: false
+    }
+  
+    addSecond = () => {
+      this.setState((prevState) => {
+        return { time: prevState.time + 1 }
+      })
+    }
+  
+    handleClick = () => {
+      if (this.state.active) {
+        clearInterval(this.idInterval)
+      } else {
+        this.idInterval = setInterval(() => this.addSecond(), 1000);
+      }
+  
+      this.setState({
+        active: !this.state.active
+      });
+  
+    }
+  
+    render() {
+      return (
+        <>
+          <p>{this.state.time}</p>
+          <SwitchButton click={this.handleClick} active={this.state.active}/>
+        </>
+      );
+    }
+}
+ 
+export default Counter;
